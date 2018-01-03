82c46ab9758d72ea6608fc4957ea9b4c833c2b21
Catlin Zilinski <cat@codecatz.com>
2018-01-02 19:57:06 -0800

    Working on making CoreSystemComponent

---
+++ Away Home/Assets/_Game/Scripts/Actors/Common/CoreSystemComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreSystemComponent : MonoBehaviour {
+
+	#region DELEGATES
+    /// <param name="change">If less than 0, then indicates allocated resource loss, otherwise indicates idle resource gain.</param>
+	public delegate void ComputerResourcesChanged(float change);
+	/// <summary>Fired when allocated computer resources are lost or gained.</summary>
+    public event ComputerResourcesChanged onComputerResourcesChanged;
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
