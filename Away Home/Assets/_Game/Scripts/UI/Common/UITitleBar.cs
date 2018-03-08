ec22fefd78c4b1cf1ea9fc34af807104303752e3
Catlin Zilinski <catlin@rooof.com>
2018-03-08 08:20:23 -0800

    Working on UI stuff
    -Working on laying out the UIWindow
    - Learned should use special auto layout classes.

---
+++ Away Home/Assets/_Game/Scripts/UI/Common/UITitleBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITitleBar : MonoBehaviour {
+
+    #region FIELDS
+    /** The text field for the title bar, if any */
+    public Text text;
+
+    #endregion FIELDS
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Away Home/Assets/_Game/Scripts/UI/Common/UITypes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UISize
+{
+    Auto, Tiny, Small, Medium, Large, Full
+}
